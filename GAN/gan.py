6683e85c0f9bb7324dc7afd3c19fdbd908e3559b
jwkweon <02kjw0203@gmail.com>
2023-04-24 18:34:24 +0900

    init code

--- GAN/gan.py
+++ GAN/gan.py
@@ -1,2 +1,79 @@
 import torch
-import numpy as np
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self, dataset='mnist'):
+        super(Generator, self).__init__()
+        self.z_dim = 100
+        if dataset == 'mnist' or dataset == 'fashion':
+            self.ch = 1
+        else:
+            self.ch = 3
+        
+        self.n_out = 32*32*self.ch
+        
+        self.fc0 = nn.Sequential(
+                nn.Linear(self.z_dim, 256),
+                nn.ReLU()
+            )
+        self.fc1 = nn.Sequential(
+                nn.Linear(256, 512),
+                nn.ReLU()
+            )
+        self.fc2 = nn.Sequential(
+                nn.Linear(512, 1024),
+                nn.ReLU()
+            )
+        self.fc3 = nn.Sequential(
+                nn.Linear(1024, self.n_out),
+                nn.Tanh()
+            )
+
+    def forward(self, x):
+        x = self.fc0(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        
+        x = x.view(-1, self.ch, 32, 32)
+        return x
+    
+class Discriminator(nn.Module):
+    def __init__(self, dataset='mnist'):
+        super(Discriminator, self).__init__()
+        
+        if dataset == 'mnist' or dataset == 'fashion':
+            self.ch = 1
+        else:
+            self.ch = 3
+            
+        self.n_in = 32*32*self.ch
+        self.n_out = 1
+        self.fc0 = nn.Sequential(
+                    nn.Linear(self.n_in, 1024),
+                    nn.ReLU(),
+                    nn.Dropout(0.3)
+                    )
+        self.fc1 = nn.Sequential(
+                    nn.Linear(1024, 512),
+                    nn.ReLU(),
+                    nn.Dropout(0.3)
+                    )
+        self.fc2 = nn.Sequential(
+                    nn.Linear(512, 256),
+                    nn.ReLU(),
+                    nn.Dropout(0.3)
+                    )
+        self.fc3 = nn.Sequential(
+                    nn.Linear(256, self.n_out),
+                    nn.Sigmoid()
+                    )
+    
+    def forward(self, x):
+        x = x.view(-1, self.n_in)
+        x = self.fc0(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
--- GAN/train.py
+++ GAN/train.py
@@ -1,6 +1,13 @@
+import os
+import random
+
 import torch
+import torch.nn as nn
+from gan import Generator, Discriminator
 from dataloader import Data_simple
 from torch.utils.data import DataLoader
+
+import torch.optim as optim
 import numpy as np
 
 def print_stats(dataset):
@@ -38,9 +45,82 @@ def print_stats(dataset):
         print(f'max: {max_val}')
         print(f'mean: {mean_val}')
         print(f'std: {std_val}')
-        
-train_dataset = Data_simple(True, "mnist")
-train_loader = DataLoader(train_dataset, batch_size=4, num_workers=4, shuffle=True)
+
+
+def make_noise(n, z_dim=100):
+    return torch.randn(n, z_dim)
+
+def make_ones(size):
+    return torch.ones(size, 1)
+
+def make_zeros(size):
+    return torch.zeros(size, 1)
+
+
+device = 'cuda'
+
+data = 'mnist' # ["mnist", "fashion", "cifar10"]
+n_batch = 128
+n_epochs = 500
+n_samples = 36
+z_dim = 100
+
+
+train_dataset = Data_simple(True, data)
+train_loader = DataLoader(train_dataset, batch_size=n_batch, num_workers=4, shuffle=True, drop_last=True)
 
 # print_stats(train_dataset)
 
+generator = Generator(data)
+discriminator = Discriminator(data)
+
+generator.to(device)
+discriminator.to(device)
+
+g_optim = optim.SGD(generator.parameters(), lr=0.01, momentum=0.9)
+d_optim = optim.SGD(discriminator.parameters(), lr=0.01, momentum=0.9)
+
+# TO-DO : use wandb to log
+g_losses = []
+d_losses = []
+
+loss_fn = nn.BCELoss()
+
+fixed_z = make_noise(n_samples, z_dim).to(device)
+
+real_label = 1.
+fake_label = 0.
+
+print(f"Starting Training...")
+
+for epoch in range(1):
+    for i, data in enumerate(train_loader):
+        imgs, _ = data
+        imgs = imgs.to(device)
+        
+        # update D : max log(D(x)) + log(1-D(G(z)))
+        d_optim.zero_grad()
+        
+        label = make_ones(n_batch).to(device)
+        print(imgs.shape)
+        output = discriminator(imgs)
+        
+        D_loss_real = loss_fn(output, label)
+        D_loss_real.backward()
+        D_x = output.mean().item()
+        
+        z = make_noise(n_batch, z_dim).to(device)
+        fake = generator(z)
+        
+        label = make_zeros(n_batch).to(device)
+        output = discriminator(fake.detach())
+        
+        D_loss_fake = loss_fn(output, label)
+        D_loss_fake.backward()
+        D_G_z = output.mean().item()
+        
+        D_loss = D_loss_real + D_loss_fake
+        
+        d_optim.step()
+        
+        print(f'loss D : {D_loss}')
