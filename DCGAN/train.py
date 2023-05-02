4932b4e5d5d6c338989d900802305df3e79e7f34
jwkweon <02kjw0203@gmail.com>
2023-05-02 17:59:21 +0900

    add argparse code (-ing)

--- DCGAN/train.py
+++ DCGAN/train.py
@@ -4,6 +4,7 @@ import time
 import math
 import numpy as np
 import imageio
+import argparse
 
 import torch
 import torch.nn as nn
@@ -60,93 +61,103 @@ def save_gifs(n_samples, images, num_epochs, data):
     imgs = [np.array(to_image(i)) for i in images]
     imageio.mimsave(path_save + f'{num_epochs}_gif_results_{data}.gif', imgs)
 
-device = "cuda:0"
 
-dataset_name = 'cifar10' 
-n_batch = 1024
-n_epochs = 1000
-n_samples = 36
-z_dim = 100
-to_image = ToPILImage()
-
-if dataset_name == 'cifar10':
-    train_dataset = Data_simple(True, dataset_name)
-    generator = Generator_cifar10()
-    discriminator = Discriminator_cifar10()
-else: # 'lsun', 'imagenet'
-    raise Exception('Dataset is not prepared!')
-
-train_loader = DataLoader(train_dataset, batch_size=n_batch, num_workers=4, shuffle=True, drop_last=True)
-
-
-generator.to(device)
-discriminator.to(device)
-
-g_optim = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-d_optim = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-
-# TO-DO : use wandb to log
-g_losses = []
-d_losses = []
-
-img_for_gif = []
-
-loss_fn = nn.BCELoss()
-
-fixed_z = make_noise(n_samples, z_dim).to(device)
-
-print(f"Starting Training...")
-start_time = time.time()
-
-for epoch in range(n_epochs):
-    for i, data in enumerate(train_loader):
-        imgs, _ = data
-        imgs = imgs.to(device)
-        
-        # update D : max log(D(x)) + log(1-D(G(z)))
-        d_optim.zero_grad()
-        
-        label = make_ones(n_batch).to(device)
-        output = discriminator(imgs)
-        
-        D_loss_real = loss_fn(output, label)
-        D_loss_real.backward()
-        D_x = output.mean().item()
-        
-        z = make_noise(n_batch, z_dim).to(device)
-        fake = generator(z)
-        
-        label = make_zeros(n_batch).to(device)
-        output = discriminator(fake.detach())
-        
-        D_loss_fake = loss_fn(output, label)
-        D_loss_fake.backward()
-        D_G_z = output.mean().item()
-        
-        D_loss = D_loss_real + D_loss_fake
-        
-        d_optim.step()
-        
-        # update G : max log(D(G(z)))
-        g_optim.zero_grad()
-        
-        label = make_ones(n_batch).to(device)
-        output = discriminator(fake)
-
-        G_loss = loss_fn(output, label)
-        G_loss.backward()
-        
-        g_optim.step()
-        
-        if i % 20 == 0:
-            print(f"[{epoch+1}/{n_epochs}][{i}/{len(train_loader)}][{time.time()-start_time:.4f}s]\
-                    Loss_D: {D_loss:.4f}, Loss_G: {G_loss:.4f},\
-                    D(x): {D_x:.4f}, D(G(x)): {D_G_z:.4f}")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_name', type=str, default='cifar10', help='dataset name: ["cifar10"]')
+    parser.add_argument('--n_batch', type=int, help='number of batch', default=1024)
+    parser.add_argument('--n_epochs', type=int, help='epochs to train', default=1000)
+    parser.add_argument('--n_samples', type=int, default=36)
+    parser.add_argument('--z_dim', type=int, help='number of worker processes', default=100)
+    args = parser.parse_args()
     
-    samples = generator(fixed_z)
-    img_for_gif.append(make_grids(n_samples, samples.detach()))
-    
-    if (epoch+1) % 10 == 0:
-        save_results(n_samples, samples.detach(), epoch+1, dataset_name)
-    
-save_gifs(n_samples, img_for_gif, n_epochs, dataset_name)
+    device = "cuda:0"
+
+    # dataset_name = 'cifar10' 
+    # n_batch = 1024
+    # n_epochs = 1000
+    # n_samples = 36
+    # z_dim = 100
+    to_image = ToPILImage()
+
+    if dataset_name == 'cifar10':
+        train_dataset = Data_simple(True, dataset_name)
+        generator = Generator_cifar10()
+        discriminator = Discriminator_cifar10()
+    else: # 'lsun', 'imagenet'
+        raise Exception('Dataset is not prepared!')
+
+    train_loader = DataLoader(train_dataset, batch_size=n_batch, num_workers=4, shuffle=True, drop_last=True)
+
+
+    generator.to(device)
+    discriminator.to(device)
+
+    g_optim = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    d_optim = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+    # TO-DO : use wandb to log
+    g_losses = []
+    d_losses = []
+
+    img_for_gif = []
+
+    loss_fn = nn.BCELoss()
+
+    fixed_z = make_noise(n_samples, z_dim).to(device)
+
+    print(f"Starting Training...")
+    start_time = time.time()
+
+    for epoch in range(n_epochs):
+        for i, data in enumerate(train_loader):
+            imgs, _ = data
+            imgs = imgs.to(device)
+            
+            # update D : max log(D(x)) + log(1-D(G(z)))
+            d_optim.zero_grad()
+            
+            label = make_ones(n_batch).to(device)
+            output = discriminator(imgs)
+            
+            D_loss_real = loss_fn(output, label)
+            D_loss_real.backward()
+            D_x = output.mean().item()
+            
+            z = make_noise(n_batch, z_dim).to(device)
+            fake = generator(z)
+            
+            label = make_zeros(n_batch).to(device)
+            output = discriminator(fake.detach())
+            
+            D_loss_fake = loss_fn(output, label)
+            D_loss_fake.backward()
+            D_G_z = output.mean().item()
+            
+            D_loss = D_loss_real + D_loss_fake
+            
+            d_optim.step()
+            
+            # update G : max log(D(G(z)))
+            g_optim.zero_grad()
+            
+            label = make_ones(n_batch).to(device)
+            output = discriminator(fake)
+
+            G_loss = loss_fn(output, label)
+            G_loss.backward()
+            
+            g_optim.step()
+            
+            if i % 20 == 0:
+                print(f"[{epoch+1}/{n_epochs}][{i}/{len(train_loader)}][{time.time()-start_time:.4f}s]\
+                        Loss_D: {D_loss:.4f}, Loss_G: {G_loss:.4f},\
+                        D(x): {D_x:.4f}, D(G(x)): {D_G_z:.4f}")
+        
+        samples = generator(fixed_z)
+        img_for_gif.append(make_grids(n_samples, samples.detach()))
+        
+        if (epoch+1) % 10 == 0:
+            save_results(n_samples, samples.detach(), epoch+1, dataset_name)
+        
+    save_gifs(n_samples, img_for_gif, n_epochs, dataset_name)
